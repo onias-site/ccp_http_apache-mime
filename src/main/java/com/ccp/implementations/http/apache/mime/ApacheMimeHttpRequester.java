3a79784d6f8dae57bb3336822feeba7e90633f2f
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-03-16 20:42:28 -0300

    login

--- src/main/java/com/ccp/implementations/http/apache/mime/ApacheMimeHttpRequester.java
+++ src/main/java/com/ccp/implementations/http/apache/mime/ApacheMimeHttpRequester.java
@@ -2,10 +2,13 @@ package com.ccp.implementations.http.apache.mime;
 
 import java.util.Set;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 
@@ -26,10 +29,9 @@ class ApacheMimeHttpRequester implements CcpHttpRequester {
 			String header = headers.getDynamicVersion().getAsString(headerName);
 			metodo.addHeader(headerName, header);
 		}
-		
+	
 		try {
 			CloseableHttpClient client = CcpHttpRequestRetryHandler.getClient();
-
 			CloseableHttpResponse response = client.execute(metodo);
 
 			HttpEntity entity = response.getEntity();
@@ -41,11 +43,47 @@ class ApacheMimeHttpRequester implements CcpHttpRequester {
 			
 			StatusLine statusLine = response.getStatusLine();
 			int statusCode = statusLine.getStatusCode();
-			CcpHttpResponse ccpHttpResponse = new CcpHttpResponse(string, statusCode);
+			String curl = this.toCurl(metodo);
+			CcpHttpResponse ccpHttpResponse = new CcpHttpResponse(string, statusCode, curl);
 			return ccpHttpResponse;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	
+	public String toCurl(HttpUriRequest request) throws Exception {
+        StringBuilder curl = new StringBuilder("curl");
+
+        // Método
+        curl.append(" -X ").append(request.getMethod());
+
+        // URL
+        curl.append(" \"").append(request.getURI()).append("\"");
+
+        // Headers
+        for (Header header : request.getAllHeaders()) {
+            curl.append(" -H \"")
+                .append(header.getName()).append(": ")
+                .append(header.getValue())
+                .append("\"");
+        }
+
+        // Body (POST, PUT, PATCH...)
+        if (request instanceof HttpEntityEnclosingRequestBase) {
+            HttpEntityEnclosingRequestBase entityRequest =
+                (HttpEntityEnclosingRequestBase) request;
+
+            HttpEntity entity = entityRequest.getEntity();
+            if (entity != null) {
+                String body = EntityUtils.toString(entity);
+                curl.append(" --data '")
+                    .append(body.replace("'", "'\"'\"'")) // escape aspas simples
+                    .append("'");
+            }
+        }
+
+        return curl.toString();
+    }
 	
 }
